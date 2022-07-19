072209156c1658d1ed743b8ddf1ce8285d7c5449
SamLinux <oxedom@gmail.com>
2022-07-19 17:32:32 +0300

    CSS stable, added faviocons and svgs, working on JS Logic

---
+++ app.js
@@ -0,0 +1,48 @@
+const books = [{
+        name: `The Little Prince`,
+        author: 'Antoine de Saint-Exupéry',
+        pages: 102
+    },
+    {
+        name: `Linux for hackers`,
+        author: 'OccupytheWeb',
+        pages: 777
+    },
+    {
+        name: `Steve Jobs autobiography`,
+        author: 'Steve Jobs',
+        pages: 333
+    }
+]
+let contentArea = document.getElementById('content')
+
+const appendBook = (book) => {
+    let div = document.createElement("div")
+
+    let textName = document.createTextNode(`Name: ${book.name}`)
+    let textAuthor = document.createTextNode(`Author: ${book.author}`)
+    let textPages = document.createTextNode(`Pages: ${book.pages}`)
+
+    let pName = document.createElement("p")
+    let pAuthor = document.createElement("p")
+    let pPages = document.createElement("p")
+
+    //Appends Text Nodes to pTag
+    pName.appendChild(textName)
+    pAuthor.appendChild(textAuthor)
+    pPages.appendChild(textPages)
+
+    //Appends All Ptags to DIV
+    div.appendChild(pName);
+    div.appendChild(pAuthor)
+    div.appendChild(pPages)
+
+    //Inserts Div into the DOM
+    contentArea.appendChild(div)
+
+
+}
+
+books.forEach(book => {
+    appendBook(book)
+})
